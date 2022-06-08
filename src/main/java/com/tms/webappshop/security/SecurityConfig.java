7d12fb01b87f671542b8be057a01e91956ec2148
Aleksey Krasutsky <l.crasutscky2017@yandex.ru>
2022-06-08 19:08:33 +0300

    1

--- src/main/java/com/tms/webappshop/security/SecurityConfig.java
+++ src/main/java/com/tms/webappshop/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.tms.webappshop.security;
 
 import com.tms.webappshop.entity.PermissionsEnum;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +11,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import static com.tms.webappshop.common.Helpers.passwordEncoder;
 
 @Configuration
